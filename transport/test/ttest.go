f82ca595966e1e3719609840bd69539184e198bb
Juan Batiz-Benet <juan@benet.ai>
2015-01-20 15:02:49 -0800

    fixed data race in test

--- transport/test/ttest.go
+++ transport/test/ttest.go
@@ -350,6 +350,8 @@ func SubtestStressNSwarmNConnNStreamNMsg(t *testing.T, tr pst.Transport, nSwarm,
 		for _, a := range swarms {
 			for _, b := range swarms {
 				wg.Add(1)
+				a := a // race
+				b := b // race
 				go rateLimit(func() {
 					defer wg.Done()
 					openConnsAndRW(a, b)
